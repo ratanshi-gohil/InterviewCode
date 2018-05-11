dd8ec43637c27ad126afad1684a12cf691dde7fd
Ratanshi Gohil <rgohl@hpfc.com>
2018-05-10 21:38:35 -0400

    Added input files and created dto class for Record data

--- InterviewCode.Dto/RecordDto.cs
+++ InterviewCode.Dto/RecordDto.cs
@@ -8,5 +8,14 @@ namespace InterviewCode.Dto
 {
     public class RecordDto
     {
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Gender { get; set; }
+
+        public string FavoriteColor { get; set; }
+
+        public string DateOfBirth { get; set; }
     }
 }
